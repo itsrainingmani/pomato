7e4aca40ab9414f4688f32df997df81891e64a66
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-26 02:43:33 -0500

    Create effect that starts and stops a js interval

---
+++ src/cljs/pomato/effects.cljs
@@ -0,0 +1,11 @@
+(ns pomato.effects
+  (:require [re-frame.core :refer [reg-fx dispatch]]))
+
+(reg-fx        ;; the re-frame API for registering effect handlers
+ :interval                  ;; the effect id
+ (let [live-intervals (atom {})]                 ;; storage for live intervals
+   (fn [{:keys [action id frequency]}]     ;; the handler
+     (if (= action :start)
+       (swap! live-intervals assoc id (js/setInterval #(dispatch [:dec-time]) frequency))
+       (do (js/clearInterval (get @live-intervals id))
+           (swap! live-intervals dissoc id))))))
