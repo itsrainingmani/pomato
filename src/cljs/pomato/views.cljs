6e2673f1550446f4809b0e6388be978c71f04db7
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-26 02:42:57 -0500

    Create main page view components

---
+++ src/cljs/pomato/views.cljs
@@ -0,0 +1,34 @@
+(ns pomato.views
+  (:require
+   [pomato.util :refer [convert-sec-to-string]]
+   [re-frame.core :refer [dispatch subscribe]]
+   [pomato.subs :as subs]
+   [stylefy.core :as stylefy]))
+
+(defn start-button []
+  [:<>
+   [:button {:style {:background-color "green"}
+             :on-click (fn [] (dispatch [:pomo-start]))} "Start"]])
+
+(defn stop-button []
+  [:<>
+   [:button {:style {:background-color "red"}
+             :on-click (fn [] (dispatch [:pomo-stop]))} "Stop"]])
+
+(defn reset-button []
+  [:<>
+   [:button {:style {:background-color "grey"}} "Reset"]])
+
+(defn timer-input []
+  (let [current-time (subscribe [::subs/curtime])]
+    (fn []
+      [:div
+       [:p (convert-sec-to-string @current-time)]
+       [start-button]
+       [stop-button]
+       [reset-button]])))
+
+(defn main-panel []
+  [:div
+   [:h1 "Pomato"]
+   [timer-input]])
