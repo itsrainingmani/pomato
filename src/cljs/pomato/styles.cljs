d36af6b8fb08403d4438cb90bd6dee4ea2ef0c9e
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-30 17:29:07 -0500

    Make the current time large and use work-sans font for it.
    Add a red flashing text color keyframe

--- src/cljs/pomato/styles.cljs
+++ src/cljs/pomato/styles.cljs
@@ -5,11 +5,11 @@
                    [:0% {:background-position-x 0}]
                    [:to {:background-position-x "10240px"}])
 
-;; (stylefy/tag "body" {:background "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"
-;;                      :background-size "400% 400%"
-;;                      :animation "flowy 15s ease infinite"
-;;                      ::stylefy/vendors ["webkit" "moz" "o"]
-;;                      ::stylefy/auto-prefix #{:animation}})
+(stylefy/keyframes "flash-text"
+                   [:0% {:color "black"}]
+                   [:50% {:color "#DC352C"}]
+                   [:100% {:color "black"}])
+
 
 (stylefy/tag "body" {:margin "0 0 0 0"})
 
@@ -26,20 +26,25 @@
                         ::stylefy/mode [[:hover {:box-shadow "2px 2px 0 #222"}]
                                         [:active {:background-color "#d9e5f1"}]]})
 
-(def start-btn-style (merge general-btn-style {:background-color "rgb(131, 255, 205)"}))
+(def start-btn-style (merge general-btn-style {:background-color "rgb(22, 244, 155)"}))
 
 (def stop-btn-style (merge general-btn-style {:background-color "#DC352C"}))
 
-(def reset-btn-style (merge general-btn-style {:background-color "grey"}))
+(def reset-btn-style (merge general-btn-style {:background-color "#FCF3B0"}))
+
+(def pomo-btn-style (merge general-btn-style {:background-color "#fac7ff"}))
 
 (def app-style {:display "flex"
                 :flex-direction "column"
                 :align-items "center"
                 :font-family "sans-serif"})
 
-(def time-style {:text-align "center"
-                 :font-size "5em"
-                 :background-clip "text"})
+(def time-style {:font-family "Work Sans"
+                 :font-weight 600
+                 :text-align "center"
+                 :font-size "5.3em"
+                 :background-clip "text"
+                 :margin "40px 0 40px 0"})
 
 (def rainbow-color {:color "transparent"
                     :background-clip "text"
@@ -48,12 +53,15 @@
                     ::stylefy/vendors ["webkit" "moz" "o"]
                     ::stylefy/auto-prefix #{:animation :background-clip}})
 
+(def flash-color {:animation "flash-text 1s linear infinite"
+                  ::stylefy/vendors ["webkit" "moz" "o"]
+                  ::stylefy/auto-prefix #{:animation}})
+
 (def heading-style {
                     ;; :font-style "italic"
                     ;; :text-transform "uppercase"
                     :font-size "4em"
                     :font-weight 800
-                    :margin-bottom 0
                     :font-family "Work Sans"
                     ;; :text-shadow "rgb(26, 254, 73) 2px 4px 0px"
                     ;; :animation "rainbow 4s ease 2s infinite reverse"
