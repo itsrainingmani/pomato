edbdc2ef5adc6f4e6db5c5d8497e2072275cacb5
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-30 17:27:03 -0500

    Add a timer-type and timer-dur key to the rfdb

--- src/cljs/pomato/db.cljs
+++ src/cljs/pomato/db.cljs
@@ -1,4 +1,9 @@
 (ns pomato.db)
 
 (defonce rfdb
-  {:cur-time 1500})
+  {:cur-time 1500
+   :default-time 1500
+   :timer-type :classic
+   :timer-dur {:class 1500
+               :long  600
+               :short 300}})
