c359a7ae04b1170ff5a16f8dfb5f85e32cc9da90
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-30 15:42:52 -0500

    Add rainbow gradient text animation.
    * Make text bigger for headings and p

--- src/cljs/pomato/styles.cljs
+++ src/cljs/pomato/styles.cljs
@@ -2,10 +2,8 @@
   (:require [stylefy.core :as stylefy]))
 
 (stylefy/keyframes "rainbow"
-                    [:20% {:text-shadow "2px 4px 0px #ff00a0"}]
-                    [:40% {:text-shadow "2px 4px 0px #fe75fe"}]
-                    [:60% {:text-shadow "2px 4px 0px #ff0000"}]
-                    [:80% {:text-shadow "2px 4px 0px #73fffe"}])
+                   [:0% {:background-position-x 0}]
+                   [:to {:background-position-x "10240px"}])
 
 ;; (stylefy/tag "body" {:background "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"
 ;;                      :background-size "400% 400%"
@@ -16,15 +14,15 @@
 (stylefy/tag "body" {:margin "0 0 0 0"})
 
 
-(def general-btn-style {:border-radius "5px"
+(def general-btn-style {:border-radius "6px"
                         :font-weight 600
                         :line-height 1
                         :color "rgb(34, 34, 34)"
                         :border "2px solid #222"
-                        :box-shadow "4px 4px 0 #222"
-                        :margin-right "6px"
+                        :box-shadow "5px 5px 0 #222"
+                        :margin-right "8px"
                         :padding "0.09em 0.5em 0.09em"
-                        :font-size "larger"
+                        :font-size "2em"
                         ::stylefy/mode [[:hover {:box-shadow "2px 2px 0 #222"}]
                                         [:active {:background-color "#d9e5f1"}]]})
 
@@ -40,13 +38,23 @@
                 :font-family "sans-serif"})
 
 (def time-style {:text-align "center"
-                 :font-size "x-large"})
+                 :font-size "5em"
+                 :background-clip "text"})
 
+(def rainbow-color {:color "transparent"
+                    :background-clip "text"
+                    :background-image "linear-gradient(270deg,#1e5799,#2ce0bf,#76dd2c,#dba62b,#ff1493,#1e5799)"
+                    :animation "rainbow 64s linear infinite"
+                    ::stylefy/vendors ["webkit" "moz" "o"]
+                    ::stylefy/auto-prefix #{:animation :background-clip}})
 
 (def heading-style {
                     ;; :font-style "italic"
                     ;; :text-transform "uppercase"
-                    :font-size "32pt"
+                    :font-size "4em"
+                    :font-weight 800
+                    :margin-bottom 0
+                    :font-family "Work Sans"
                     ;; :text-shadow "rgb(26, 254, 73) 2px 4px 0px"
                     ;; :animation "rainbow 4s ease 2s infinite reverse"
                     })
