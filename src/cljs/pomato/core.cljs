06ca8eebee2a3537bbb5f0d0d31d0ebeacd75fa0
Manikandan Sundararajan <mani@hey.com>
2021-02-02 00:10:17 -0500

    Add re-pressed event-listener

--- src/cljs/pomato/core.cljs
+++ src/cljs/pomato/core.cljs
@@ -8,7 +8,6 @@
    [stylefy.core :as stylefy]
    [re-pressed.core :as rp]))
 
-
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
@@ -21,6 +20,9 @@
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
+  (re-frame/dispatch-sync [::rp/add-keyboard-event-listener "keypress"])
+  (re-frame/dispatch [::rp/set-keypress-rules
+                      {:event-keys [[[::events/test] [{:keyCode 32}]]]}])
   (dev-setup)
   (stylefy/init)
   (mount-root))
