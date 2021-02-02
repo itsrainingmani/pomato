dfa6a2d72c87c3ca6c90f2b075eb5cc20cb81f16
Manikandan Sundararajan <mani@hey.com>
2021-02-01 23:15:21 -0500

    Move key press detection function to util.cljs

--- src/cljs/pomato/util.cljs
+++ src/cljs/pomato/util.cljs
@@ -1,6 +1,9 @@
 (ns pomato.util
   (:require [goog.string :as gstring]
-            [goog.string.format]))
+            [goog.string.format]
+            [goog.events.KeyCodes :as keycodes]
+            [goog.events :as gev])
+  (:import [goog.events EventType KeyHandler]))
 
 (defn convert-sec-to-string
   "Converts seconds into a string"
@@ -8,3 +11,22 @@
   (let [mins (quot seconds 60)
         secs (rem seconds 60)]
     (str (gstring/format "%02d" mins) ":" (gstring/format "%02d"  secs))))
+
+(defn capture-key
+  "Given a `keycode`, execute function `f` "
+  [keycode-map]
+  (let [key-handler (KeyHandler. js/document)
+        press-fn (fn [key-press]
+                   (when-let [f (get keycode-map (.. key-press -keyCode))]
+                     (f)))]
+    (gev/listen key-handler
+                (-> KeyHandler .-EventType .-KEY)
+                press-fn)))
+
+(defn app-keybindings []
+    ;; sets up the event listener
+  (capture-key {keycodes/l #(js/alert "Luna Lovegood")
+                keycodes/d #(js/alert "Dumbledore")
+                keycodes/h #(js/alert "Hermione")
+                keycodes/y #(js/alert "harrY")
+                keycodes/r #(js/alert "Ron")}))
