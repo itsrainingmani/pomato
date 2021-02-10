65235f92e3ce7ecfa8eb1720461b5813f09dc164
Manikandan Sundararajan <mani@hey.com>
2021-02-10 08:46:36 -0500

    Destructure cofx to the db

--- src/cljs/pomato/events.cljs
+++ src/cljs/pomato/events.cljs
@@ -12,20 +12,20 @@
 
 (rf/reg-event-fx
  :pomo-start
- (fn [cofx _]
-   (when-not (or (zero? (get-in cofx [:db :cur-time] 0)) (true? (get-in cofx [:db :is-timer?])))
+ (fn [{:keys [db]} _]
+   (when-not (or (zero? (get db :cur-time)) (true? (get db :is-timer?)))
      {:interval {:action :start
                  :id :pomo-timer
                  :frequency 1000}
-      :db (assoc (:db cofx) :is-timer? true)})))
+      :db (assoc db :is-timer? true)})))
 
 (rf/reg-event-fx
  :pomo-stop
- (fn [cofx _]
-   (when (true? (get-in cofx [:db :is-timer?]))
+ (fn [{:keys [db]} _]
+   (when (true? (get db :is-timer?))
      {:interval {:action :cancel
                  :id :pomo-timer}
-      :db (assoc (:db cofx) :is-timer? false)})))
+      :db (assoc db :is-timer? false)})))
 
 (rf/reg-event-fx
  :pomo-reset
